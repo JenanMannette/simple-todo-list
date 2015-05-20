7a2d77ffb82e9a62a197998a0274b841da1ec3a1
Jenan Mannette <jenanmannette@gmail.com>
2015-05-20 11:32:28 -0600

    attempt at stretch goals, but didn't get them

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var input = document.getElementById("input");
 var container = document.getElementById("container");
 var button = document.getElementById("add_todo");
-var list = document.getElementById("list");
 
 var addItem = function(){
   var text = input.value;
-  container.innerHTML = container.innerHTML + "<li>" + text + "</li>";
+  container.innerHTML = container.innerHTML + "<li>" + text + "</li>"
   input.value = "";
+  //need to make new li have the same "notCompleted" class
 }
+
+var strike = function(){
+      li[i].classList.remove("notCompleted");
+      li[i].classList.add("completed");
+    }
+
+li.addEventListener("click", function(){
+  strike();
+});
+
+//when li is clicked, it should strikeout
+  //will strikeout when class changes to "completed"
+  //need this to toggle from "completed"/"not completed"
